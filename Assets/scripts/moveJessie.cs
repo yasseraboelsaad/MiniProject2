105468e259b241104994f738fdbbe4a20c069df8
Yasser Aboel-Saad <yasser.aboelsaad@gmail.com>
2016-11-30 20:01:30 +0100

    added trigger support and fixed some animations

---
+++ Assets/scripts/moveJessie.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveJessie : MonoBehaviour {
+
+	Animator myAnim;
+
+	// Use this for initialization
+	void Start () {
+		myAnim = GetComponent <Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("q")){
+			myAnim.SetTrigger ("SleepTrigger");
+		}
+		if(Input.GetKey("e")){
+			myAnim.SetTrigger ("WakeUpTrigger");
+		}
+	}
+}
